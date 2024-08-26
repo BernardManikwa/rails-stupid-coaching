4edf5f95d87e202eb6875ca49f001757f4ece8b2
bernardmanikwa <bernard.manikwa@outlook.com>
2024-08-26 16:57:19 +0200

    stupid coaching

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,13 @@
+# app/controllers/pages_controller.rb
+class PagesController < ApplicationController
+  def home
+  end
+
+  def about
+  end
+
+  def ask
+    @ask
+  end
+
+end
---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,21 @@
+class QuestionsController < ApplicationController
+    def questions
+      @question = params[:question]
+      @questions = ["Hello", "Can I go shopping?", "I am going to work"]
+
+      if params[:question]
+        @questions = @questions.select { |question| question.downcase.include?(params[:question].downcase) }
+      end
+    end
+
+    def answer
+      @question = params[:question]
+      if @question.ends_with?("?")
+        @answer = "Silly question, get dressed and go to work!"
+        elsif @question ==  "I am going to work"
+        @answer = "Great!"
+      else
+        @answer = "I don't care, get dressed and go to work!"
+      end
+    end
+  end
